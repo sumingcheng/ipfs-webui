a76436a4e7af313782dfbb8727f3d024fc52f357
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-01-03 11:41:23 +0000

    fix: 路由固定

--- src/layout/menu.js
+++ src/layout/menu.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import React, { useState, useRef } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
 import Main from './main.js'
 import MenuRight from './components/menuRight.js'
 import { Col, Row, Menu } from 'antd'
@@ -8,12 +8,20 @@ import { Col, Row, Menu } from 'antd'
 import './layout.css'
 
 const App = () => {
-  const [current, setCurrent] = useState('index')
+  const [current, setCurrent] = useState('/modelRepository')
+  const modelRepositoryRef = useRef(null) // 创建一个 ref
 
   const onClick = (e) => {
     console.log('click ', e)
     setCurrent(e.key)
   }
+
+  React.useEffect(() => {
+    // 模拟点击
+    if (modelRepositoryRef.current) {
+      modelRepositoryRef.current.click()
+    }
+  }, [])
 
   // 定义菜单项
   const menuItems = [
@@ -22,10 +30,11 @@ const App = () => {
       key: 'index'
     },
     {
-      label: <Link to="/modelRepository">模型仓库</Link>,
+      label: <Link to="/modelRepository" ref={modelRepositoryRef}>模型仓库</Link>,
       key: 'modelRepository'
     }
   ]
+  /* 初始选中 */
 
   return (
     <Router>
